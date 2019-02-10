bbb2dfa8dd5a30364f2ba01fdc9e63adb21b4e06
reinaldomaslim <reinaldomaslim@gmail.com>
2019-02-10 13:51:10 +0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install gpsd gpsd-clients
+sudo apt-get install gnome-terminal
+sudo apt-get install python-qt4
+sudo apt-get install python-pip
+pip install googlemaps
+pip install gmplot
+
+
+#### set autorun via .bashrc, 
+#remove login by adding /usr/share/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf file. 
+#greeter-session=lightdm-gtk-greeter 
+#autologin-user=srackham 
+#go to settings and enable autologin
+
+
