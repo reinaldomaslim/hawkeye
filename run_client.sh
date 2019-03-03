7009d7f58afaee361186ede3f8abccd6e2b615cd
reinaldomaslim <reinaldomaslim@gmail.com>
2019-03-03 13:41:26 +0800

    modify working directory

--- run_client.sh
+++ run_client.sh
@@ -5,5 +5,5 @@ echo "project of reinaldomaslim"
 #sleep wait for gps to be up
 sleep 10
 
-gnome-terminal --tab -e "python ~/Documents/hawkeye/client_node_1.py"
-gnome-terminal --tab -e "python ~/Documents/hawkeye/client_node_2.py"
+gnome-terminal --tab -e "python client_node_1.py"
+gnome-terminal --tab -e "python client_node_2.py"
--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@ echo "Hawkeye Server"
 echo "project of reinaldomaslim"
 
 #sleep wait for wifi to be up
-sleep 5
+# sleep 5
 
-gnome-terminal --tab -e "python ~/Documents/hawkeye/server_node_1.py"
-gnome-terminal --tab -e "python ~/Documents/hawkeye/server_node_2.py"
+gnome-terminal --tab -e  "python server_node_1.py" \
+               --tab -e  "python server_node_2.py"
