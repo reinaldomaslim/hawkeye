f018e405acd9424c6318c2772ecbc1246e1b3313
reinaldomaslim <reinaldomaslim@gmail.com>
2019-04-12 15:04:39 +0800

    wait for network

--- run_client.sh
+++ run_client.sh
@@ -2,14 +2,20 @@
 echo "Hawkeye Client"
 echo "project of reinaldomaslim"
 
+#check for internet, so time is correct
+sleep 2
+
 sudo systemctl stop gpsd.socket
 sudo systemctl disable gpsd.socket
 sudo killall gpsd
 sudo gpsd  /dev/ttyUSB0 -F  /var/run/gpsd.sock
 
 #sleep wait for gps to be up
-sleep 5
+echo "wait for gps"
+sleep 2
 gnome-terminal --tab --working-directory=$HOME/Documents/hawkeye/ -e "python client_node_1.py"
+
+cgps
 
 #gnome-terminal --tab --working-directory=$HOME/Documents/hawkeye/ -e "python client_node_1.py" \
 #               --tab --working-directory=$HOME/Documents/hawkeye -e "python client_node_2.py"
