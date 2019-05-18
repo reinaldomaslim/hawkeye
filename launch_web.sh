f19640ac7837b19b46cc7e5002d3c91971b78175
reinaldomaslim <reinaldomaslim@gmail.com>
2019-05-18 09:15:49 +0800

    just tryin

--- launch_web.sh
+++ launch_web.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source virtual/bin/activate
-gsutil rm -r gs://staging.neon-bank-181705.appspot.com 
-gcloud app deploy --quiet --project neon-bank-181705 
+gcloud app deploy --quiet --project neon-bank-181705 --stop-previous-version
